f4e244c67807c5da190f7b8ac1b0b11886a503c0
obslsessed <obslsessed@protonmail.com>
2023-06-19 06:51:48 +0200

    move create thread to a function

--- src/commands/chat.rs
+++ src/commands/chat.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use nerd2::CHARACTERS_PATH;
 use nerd2::{get_characters, send_chat};
 use nerd2::{Character, Context, Error};
-use poise::serenity_prelude::{self as serenity, MessageId};
+use poise::serenity_prelude::{self as serenity, GuildChannel, MessageId};
 use std::fs::read_to_string;
 
 /// chat
@@ -10,15 +10,25 @@ use std::fs::read_to_string;
 pub async fn chat(ctx: Context<'_>) -> Result<(), Error> {
     let channel = ctx.channel_id();
     let (character, message_id) = choose_character(ctx).await?;
-    let thread = channel
-        .create_public_thread(ctx, message_id, |thread| thread.name(&character.name))
-        .await?;
+    let thread = create_thread(&ctx, &message_id, &character.name).await?;
     let reply = thread.await_reply(ctx).await.unwrap(); // TODO: make it not unwrap
     let text = &reply.content;
     dbg!(&reply);
     let answer = send_chat(text).await?;
     thread.say(ctx, answer).await?;
     Ok(())
+}
+
+async fn create_thread(
+    ctx: &Context<'_>,
+    message_id: &MessageId,
+    name: &str,
+) -> Result<GuildChannel> {
+    let channel = ctx.channel_id();
+    let thread = channel
+        .create_public_thread(ctx, message_id, |thread| thread.name(name))
+        .await?;
+    Ok(thread)
 }
 
 async fn choose_character(ctx: Context<'_>) -> Result<(Character, MessageId), Error> {
