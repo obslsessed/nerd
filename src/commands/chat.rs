894359c3efbe745bc5470fccf9b45cca82a3479b
obslsessed <obslsessed@protonmail.com>
2023-06-21 06:21:24 +0200

    handle_chat function, reads chat information from file (for 1 message for now)

--- src/commands/chat.rs
+++ src/commands/chat.rs
@@ -1,27 +1,61 @@
 use anyhow::Result;
-use nerd2::CHARACTERS_PATH;
-use nerd2::{get_characters, send_chat};
+use async_openai::types::{ChatCompletionRequestMessageArgs, CreateChatCompletionRequest, Role};
+use nerd2::{get_characters, new_chat, send_chat};
 use nerd2::{Character, Context, Error};
-use poise::serenity_prelude::{self as serenity, GuildChannel, MessageId};
-use std::fs::read_to_string;
+use nerd2::{CHARACTERS_PATH, CONVERSATIONS_PATH};
+use poise::serenity_prelude::{
+    self as serenity, CacheHttp, ChannelId, GuildChannel, Http, MessageId,
+};
+use std::fs::{read_to_string, write};
 
 /// chat
 #[poise::command(slash_command, prefix_command)]
 pub async fn chat(ctx: Context<'_>) -> Result<(), Error> {
-    let channel = ctx.channel_id();
     let (character, message_id) = choose_character(ctx).await?;
     let thread = create_thread(&ctx, &message_id, &character.name).await?;
+    let thread_id = thread.id;
+    new_chat(&character, thread_id)?;
     let reply = thread.await_reply(ctx).await.unwrap(); // TODO: make it not unwrap
-    let text = &reply.content;
+    let reply_text = &reply.content;
     dbg!(&reply);
-    let answer = send_chat(text, character).await?;
-    thread.say(ctx, answer).await?;
+    handle_chat(ctx, reply_text, thread, character).await?;
+
+    Ok(())
+}
+
+async fn handle_chat(
+    http: impl CacheHttp,
+    input: &str,
+    thread: impl Into<ChannelId>,
+    character: Character,
+) -> Result<()> {
+    let id = thread.into();
+    let ctx = http.http();
+
+    let path = format!("{CONVERSATIONS_PATH}/{id}");
+    let string = read_to_string(&path)?;
+    let mut chat = serde_json::from_str::<CreateChatCompletionRequest>(&string)?;
+    let input = ChatCompletionRequestMessageArgs::default()
+        .role(Role::User)
+        .content(input)
+        .build()?;
+    chat.messages.push(input);
+    dbg!(&chat);
+    let answer = send_chat(chat.clone(), character).await?;
+    id.say(ctx, &answer).await?;
+    let output = ChatCompletionRequestMessageArgs::default()
+        .role(Role::Assistant)
+        .content(answer)
+        .build()?;
+    chat.messages.push(output);
+    let output_json = serde_json::to_string(&chat)?;
+    write(&path, output_json)?;
     Ok(())
 }
 
 async fn create_thread(
     ctx: &Context<'_>,
-    message_id: &MessageId,
+    message_id: impl Into<MessageId>,
     name: &str,
 ) -> Result<GuildChannel> {
     let channel = ctx.channel_id();
@@ -70,7 +104,7 @@ async fn choose_character(ctx: Context<'_>) -> Result<(Character, MessageId), Er
         .await?;
     let path = format!("{CHARACTERS_PATH}/{name}");
     let string = read_to_string(path)?;
-    let character = serde_json::from_str::<Character>(&string)?;
+    let character = serde_json::from_str(&string)?;
     let message_id = message.id;
-    Ok::<(Character, MessageId), Error>((character, message_id))
+    Ok((character, message_id))
 }
