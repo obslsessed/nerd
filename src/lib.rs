f32aa64cf156a290b15623db2110c0467a531fb0
obslsessed <obslsessed@protonmail.com>
2023-06-22 20:32:11 +0200

    unused import lol

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use std::{
     fs::{create_dir, read_dir, read_to_string, write},
     io::ErrorKind,
     str::FromStr,
-    sync::Arc,
 };
 
 use anyhow::Result;
@@ -99,7 +98,7 @@ pub async fn send_chat(chat: CreateChatCompletionRequest) -> Result<String> {
     let error_message = ChatCompletionResponseMessage {
         role: async_openai::types::Role::Assistant,
         content: error_text,
-    };
+    }
     let error_response = ChatChoice {
         message: error_message,
         index: 0,
